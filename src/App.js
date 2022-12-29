44614e6ea5ab07fe2dc9c071c74f80c3c79b5eaf
Nizami Sevindi <nsevindi87@gmail.com>
2022-12-29 23:14:02 +0100

    React Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
+import Home from "./Components/Home"
+import About from "./Components/About";
+import Services from "./Components/Sevices"
+import Contact from "./Components/Contact"
+import Policy from "./Components/Policy"
+import Error from "./Components/Error"
 
 const App = () =>{
   return(
     <>
-    <Switch>
-    <Route path="/" component={Home}/>
-    </Switch>
+    <Routes>   
+      <Route exact path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/policy" element={<Policy />} />
+     {/* CALISMIYOR  <Route path="/" errorElement={<Error />} /> */}
+    </Routes>
     
     </>
   )
