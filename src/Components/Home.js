f653756001bff4950d4c1045e0ef70ba65a44d88
Nizami Sevindi <nsevindi87@gmail.com>
2022-12-30 16:24:15 +0100

    Practice

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import homeImg from "../Images/homeImage.png";
+import "../Css/HomeAbout.css";
 
 const Home = ()=>{
     return(
         <>
-        <h1>Home</h1>
+        <div className="mainSection">
+            <div className="contentBox">
+                <h1>Learning made easy</h1>
+                <p>Lorem ipsum dolor desr teas das sada dasdassdfg ferd dasd fasad aasfaaf afsagrrg rlrflrf frgjsdf sdsfjsdf dfggeegaega</p>
+                <div className="btnBox">
+                    <div className="btn">
+                    <NavLink to="/about" className="readMore" >Read Me</NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className="imgContainer">
+                <img src={homeImg} alt="home"/>
+            </div>
+        </div>
         </>
     )
 }
